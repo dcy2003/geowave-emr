489fb52a4df6e4c8e357611a5b0a512331f19c56
Derek Yeager <dcy2003@gmail.com>
2016-04-20 14:05:13 -0400

    bkup queries

---
+++ queries/paris-servercql-count-server.sh
@@ -0,0 +1,1 @@
+java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-cli-debug-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain -serverCql -gwNamespace geowave.gdelt -zookeeper $HOSTNAME:2181 -instance accumulo -user geowave -password geowave -cql "BBOX(geometry,2.087,48.658,2.638,49.047)" -useAggregation
---
+++ queries/paris-spatial-count-server.sh
@@ -0,0 +1,1 @@
+java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-cli-debug-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain -bbox -gwNamespace geowave.gdelt -zookeeper $HOSTNAME:2181 -instance accumulo -user geowave -password geowave -west 2.087 -south 48.658 -east 2.638 -north 49.047 -useAggregation
