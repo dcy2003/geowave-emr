3fd7cf640c9ab2583ed6ee89eb87a5c6313cd0f5
Derek Yeager <dcy2003@gmail.com>
2016-05-11 07:43:14 -0400

    Modified ingest script for 0.9.1 CLI format

---
+++ branch-config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export CURR_BRANCH=0.9.1
--- ingest-gdelt.sh
+++ ingest-gdelt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo "Ingesting GDELT data ..."
 source geowave-env.sh
+source branch-config.sh
 
-cd ../0.9.1
+cd ../$CURR_BRANCH
 
-java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-format-gdelt-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain -localingest -f gdelt -b $STAGING_DIR/gdelt -dim spatial -cql "BBOX(geometry,-180,-90,180,90)" -gwNamespace geowave.gdelt -datastore accumulo -zookeeper $HOSTNAME:2181 -instance $INSTANCE -user geowave -password geowave -partitionStrategy round_robin -numPartitions $NUM_PARTITIONS
+# Add Store
+java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-format-gdelt-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo --gwNamespace geowave.gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
+# Add Index
+java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-format-gdelt-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addindex -t spatial gdelt-spatial --partitionStrategy round_robin --numPartitions $NUM_PARTITIONS
+# Ingest
+java -cp geowave-deploy-0.9.1-SNAPSHOT-tools.jar:geowave-format-gdelt-0.9.1-SNAPSHOT.jar mil.nga.giat.geowave.core.cli.GeoWaveMain ingest localtogw $STAGING_DIR/gdelt gdelt-accumulo gdelt-spatial -f gdelt --gdelt.cql "BBOX(geometry,-180,-90,180,90)"
