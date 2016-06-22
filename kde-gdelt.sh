143afcf2fd93526827379e0a0de01b4902cf2bbc
Derek Yeager <dcy2003@gmail.com>
2016-06-22 12:16:55 -0400

    Updates

--- kde-gdelt.sh
+++ kde-gdelt.sh
@@ -3,16 +3,18 @@
 echo "Running Geowave Yarn Script To Generate KDE ..."
 source geowave-env.sh
 
-export HADOOP_USER_CLASSPATH_FIRST=true
-export HADOOP_CLASSPATH=$(echo /data/06-20-2016/jai/*.jar /data/06-20-2016/*.jar | tr ' ' ':')
-echo "HADOOP_CLASSPATH: " ${HADOOP_CLASSPATH}
+#export HADOOP_USER_CLASSPATH_FIRST=true
+#export HADOOP_CLASSPATH=$(echo /data/06-20-2016/jai/*.jar /data/06-20-2016/*.jar | tr ' ' ':')
+#echo "HADOOP_CLASSPATH: " ${HADOOP_CLASSPATH}
 
 #java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo --gwNamespace geowave.gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
 #java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addindex -t spatial gdelt-spatial --partitionStrategy round_robin --numPartitions $NUM_PARTITIONS
 java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo-out --gwNamespace geowave.kde_gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
 
+ln -s /home/ec2-user/.geowave/0.9.1-config.properties /home/ec2-user/.geowave/unknownversion-config.properties
+
 #yarn jar geowave-tools.jar --debug analytic kde \
-yarn jar geowave-tools.jar analytic kde \
+hadoop jar geowave-tools.jar analytic kde \
 	--featureType gdeltevent \
 	--minLevel 5 \
 	--maxLevel 26 \
