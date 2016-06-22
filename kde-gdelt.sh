03a89b85386dc249641ee7d8e5a0b4ad16f3af60
Derek Yeager <dcy2003@gmail.com>
2016-06-22 11:34:21 -0400

    Updates

--- kde-gdelt.sh
+++ kde-gdelt.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-echo "Generating heatmap ..."
+
+echo "Running Geowave Yarn Script To Generate KDE ..."
 source geowave-env.sh
 
-java -cp geowave-tools.jar:geowave-format-gdelt-0.9.1.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo-out --gwNamespace geowave.kde_gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
-java -cp geowave-tools.jar:geowave-format-gdelt-0.9.1.jar mil.nga.giat.geowave.core.cli.GeoWaveMain analytic kde --featureType gdeltevent --minLevel 5 --maxLevel 26 --minSplits $NUM_PARTITIONS --maxSplits $NUM_PARTITIONS --coverageName gdeltevent_kde --hdfsHostPort ${HOSTNAME}:${HDFS_PORT} --jobSubmissionHostPort ${HOSTNAME}:${RESOURCE_MAN_PORT} --tileSize 1 gdelt-accumulo gdelt-accumulo-out
+export HADOOP_USER_CLASSPATH_FIRST=true
+export HADOOP_CLASSPATH=$(echo /data/06-20-2016/jai/*.jar /data/06-20-2016/*.jar | tr ' ' ':')
+echo "HADOOP_CLASSPATH: " ${HADOOP_CLASSPATH}
 
+#java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo --gwNamespace geowave.gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
+#java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addindex -t spatial gdelt-spatial --partitionStrategy round_robin --numPartitions $NUM_PARTITIONS
+java -cp geowave-tools.jar mil.nga.giat.geowave.core.cli.GeoWaveMain config addstore -t accumulo gdelt-accumulo-out --gwNamespace geowave.kde_gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
+
+#yarn jar geowave-tools.jar --debug analytic kde \
+yarn jar geowave-tools.jar analytic kde \
+	--featureType gdeltevent \
+	--minLevel 5 \
+	--maxLevel 26 \
+	--minSplits 24 \
+	--maxSplits 120 \
+	--coverageName gdeltevent_kde \
+	--hdfsHostPort ${HOSTNAME}:${HDFS_PORT} \
+	--jobSubmissionHostPort ${HOSTNAME}:${RESOURCE_MAN_PORT} \
+	--tileSize 1 \
+	gdelt-accumulo \
+	gdelt-accumulo-out
